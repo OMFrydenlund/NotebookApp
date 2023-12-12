e258c7bbda4dae6922930107aefe9794f64cfe23
OMFrydenlund <migfrydenlund@gmail.com>
2023-12-12 14:48:52 +0100

    from wow to HUH

--- Notebook.cs
+++ Notebook.cs
@@ -3,11 +3,42 @@ using System.Collections.Generic;
 
 namespace PersonalNotebook
 {
-    public static class Notebook
+    public class Notebook 
     {
-        private Page //define what > constructor practice
+                      //Dictionary class is built in, provides key/value pairs (each key must be unique)
+                      //Used to associate values with keys, look up values based on corresponding keys
+   
+
+        private Dictionary<string, Dictionary<int, string>> Year2024;
+            
+        public Notebook()
         {
-            //method that adheres to days/months? (2024 is the only year in use, possible array usage for set months in 2024)
+            NotebookInstance();
+        }
+
+        private void NotebookInstance()
+        //define the notebook instance
+        {
+            //apply layout then compose array of months to refer to (a set of keys, which will also have their own sets of keys(days))
+            Year2024 = new Dictionary<string, Dictionary<int, string>>;
+
+            string[] months =
+            {
+                "January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                "November", "December"
+            };
+            //for every month in the month array, create a new Dictionary 
+            //loop through months. gpt help?
+            foreach (var month in months)
+            {
+                //what in the f is this
+                Year2024[month] = new Dictionary<int, string>();
+
+                for (int day = 1; day <= 7; day++)
+                {
+                    Year2024[month][day] = "";
+                }
+            }
         }
     }
 }
